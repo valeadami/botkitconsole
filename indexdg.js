94b2434e76e2098af589b37e2cfb14a2f73f4bae
valeadami <40217408+valeadami@users.noreply.github.com>
2018-12-21 15:34:55 +0100

    con entities
    middleware con entities

--- indexdg.js
+++ indexdg.js
@@ -10,39 +10,99 @@ var controller = Botkit.consolebot({
 /********* modulo botkit middleware dialogflow */
 
 const dialogflowMiddleware = require('botkit-middleware-dialogflow')({
-    keyFilename: './botkit-test-7ea2ab19c3d7.json',  // service account private key file from Google Cloud Console
+    keyFilename: './botkit-test-43930ebfe242.json',  // service account private key file from Google Cloud Console PER QUESTO PC
   });
 
 //******************* */
 
 var bot = controller.spawn();
+
+//funzione di test 
+
+/*function myTest(bot, message, next){
+   
+    dialogflowMiddleware.receive(bot, message,function()
+    {
+        console.log('messaggio dal client '+ message.text + " con message type "+ message.type);
+        next();
+    }); 
+
+} 
+controller.middleware.receive.use(myTest); */
+
+/*Receive middleware can be used to do things like preprocess the message content using external natural language processing services like Wit.ai. 
+Additional information can be added to the message object for use down the chain.
+*/
 controller.middleware.receive.use(dialogflowMiddleware.receive);
 
+/* Send middleware can be used to do things like preprocess the message content before it gets sent out to the messaging client.*/
+
 controller.middleware.send.use(dialogflowMiddleware.use);
-controller.middleware.send.use(function(bot, message, next) {
+/*controller.middleware.send.use(function(bot, message, next) {
 
   // log the outgoing message for debugging purposes
   /*console.log('SENDING ', message.text,'TO USER', message.text);
 
-  next();*/
-  if (message.intent == 'hey') {
+  next();
+  if (message.intent == 'pippo') {
       message.text = 'Hello from DialogFlow!!!';
   }
   console.log('SENDING ', message.text,'TO USER', message.text);
   next();
 
-});
+});*/
+
 //lifecycle convo events
 controller.on('conversationStarted', function(bot, convo) {
     console.log('----------------> A conversation started with ', convo.context.user);
   });
   controller.on('conversationEnded', function(bot, convo) {
-    console.log('<----------------- A conversation ended with ', convo.context.user);
+    console.log('<----------------- A conversation ended with ', convo.context.user + " message " + convo.message);
+    
   });
-  controller.hears(['Default Welcome Intent'], 'direct_message', dialogflowMiddleware.hears, function(bot, message) {
+  //'direct_message'  'message_received'
+  controller.hears(['Default Welcome Intent'], 'message_received', dialogflowMiddleware.hears, function(bot, message) {
     var replyText = message.fulfillment.text;  // message object has new fields added by Dialogflow
+    
     bot.reply(message, replyText);
   });
-
-  
-
+  controller.hears(['test'], 'message_received', dialogflowMiddleware.hears, function(bot, message) {
+   /*
+   var replyText=getRemoteData('http://86.107.98.69:8080/AVA/rest/searchService/search_2?searchText=ciao&user=&pwd=&ava=FarmaInfoBot').then((name) => {
+      
+      replyText =name;
+     */
+    var replyText='';
+    if (message.entities) {
+      console.log('sono nelle entità');
+      for (var e = 0; e < message.entities.length; e++) {
+         console.log('nome entità '+ message.entities[e].name );
+         replyText=message.entities[e].value;
+         bot.reply(message, replyText);
+      }
+      
+  } else {
+    console.log('NON sono nelle entità');
+    replyText='casin';
+    bot.reply(message, replyText);
+  }
+    
+  //}); 
+    
+   
+  });
+//STOP FINE CONVERSAZIONE
+  controller.hears(['stopIntent'], 'message_received', dialogflowMiddleware.hears, function(bot, message) {
+    var replyText = message.fulfillment.text;
+    console.log('valore di replyText in stop da DG '+ replyText)
+   bot.startConversation(message, function(err, convo) {
+    convo.say(replyText);
+    
+   
+ 
+    convo.stop();
+  // convo.next();
+    
+    })
+    
+   });
